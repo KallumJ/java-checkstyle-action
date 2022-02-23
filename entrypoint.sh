7a60c755a5ef8edf5e506a13de2b6135a63c71c4
Kallum <57623845+KallumJ@users.noreply.github.com>
2022-02-23 11:29:46 +0000

    Added stylefile selection

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-echo "Running"
+echo "Downloading checkstyle"
 curl -LJO https://github.com/checkstyle/checkstyle/releases/download/checkstyle-9.3/checkstyle-9.3-all.jar
-curl -LJO https://raw.githubusercontent.com/checkstyle/checkstyle/master/src/main/resources/sun_checks.xml
+
+if [${INPUT_STYLFILE} -eq "google"]
+then
+    echo "Downloading selected Google stylefile"
+    curl -LJ -o stylefile.xml https://raw.githubusercontent.com/checkstyle/checkstyle/master/src/main/resources/google_checks.xml
+else
+    echo "Downloading selected Sun stylefile"
+    curl -LJ -o stylefile.xml https://raw.githubusercontent.com/checkstyle/checkstyle/master/src/main/resources/sun_checks.xml
+
 chmod +x /app/checkstyle-9.3-all.jar
-java -jar checkstyle-9.3-all.jar "${INPUT_WORKDIR}" -c sun_checks.xml
+java -jar checkstyle-9.3-all.jar "${INPUT_WORKDIR}" -c stylefile.xml
